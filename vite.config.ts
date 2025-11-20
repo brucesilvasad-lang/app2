0e6235d3c12f3211b083528e88e3c5bd585f779c
brucesilvasad-lang <brucesilva.sad@gmail.com>
2025-11-20 17:30:30 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',        // 👈 OBRIGATÓRIO PARA GITHUB PAGES
-  root: '.',         // ok
+  base: './',  // <===== ISSO CORRIGE O ERRO
   build: {
     outDir: 'dist',
   },
-  server: {
-    host: '0.0.0.0',
-  }
 });
